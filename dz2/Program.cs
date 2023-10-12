593b8b467622d77186c967e6a2ed9d70e0e5f6c9
Chinchi <chinchi1997@mail.com>
2023-10-12 12:55:02 +0300

    Add secont trusk

---
+++ dz2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая принимает на вход длину массива и в последующих строках элементы массива.
+// 5
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 3
+// 6, 1, 33 -> [6, 1, 33]
+
+
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}, ");
+     array[i] = int.Parse(Console.ReadLine());
+}
+   
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
